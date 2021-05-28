329c135fae4f64e3904f63297bb414790f91a7fe
Dawid Pawlowski <ext.dawid.pawlowski@k2.pl>
2021-05-28 15:56:53 +0200

    Deleted comments with todo

--- urlshortener/views.py
+++ urlshortener/views.py
@@ -2,18 +2,6 @@ from django.shortcuts import get_object_or_404, render
 from django.views.generic import ListView, CreateView, RedirectView
 from urlshortener.models import Link
 
-""" Co mi się nie podoba:
-    - Jak link już został skrócony to rzuca błąd walidacji, a powinno dać link który już mamy w bazie[*]
-    - Jak wpiszemy zły shortened url to dostajemy 404, powinno być chyba że np przekierwouje na homepage
-     i pisze ze nie ma takiego linka
-    - ListView jest chyba niepotrzebne, bo jest wzmianka tylko o adminie[*]
-    - Można usunąc forma jeżeli korzystam z createview dla modelu[*]
-    - Brak testów[*]
-    - Użytkownik powinien dostać pełen skrócony link a nie tylko tą końcówke[*]
-    https://studygyaan.com/django/django-everywhere-host-your-django-app-for-free-on-heroku
-    https://www.youtube.com/watch?v=Q_YOYNiSVDY
-"""
-
 
 class LinkCreateView(CreateView):
     model = Link
