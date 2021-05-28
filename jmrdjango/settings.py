a012bcab95efe5d861213840390217840b47360f
Dawid Pawlowski <ext.dawid.pawlowski@k2.pl>
2021-05-28 15:36:39 +0200

    deleted slash from allowed_hosts

--- jmrdjango/settings.py
+++ jmrdjango/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-x$nut4f3jz4=!+x^fsu&tofc*(++t(pd9=(s&4*4=2od)=-(s6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'link-shortener-jmr.herokuapp.com/',]
+ALLOWED_HOSTS = ['127.0.0.1', 'link-shortener-jmr.herokuapp.com',]
 
 
 # Application definition
